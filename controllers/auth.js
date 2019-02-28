96a6446f9ed75a4616b1bbb65013aa406732cd0c
unknown <stefaleon@gmail.com>
2019-02-28 10:03:06 +0200

    validation 1, field equality

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator/check');
 
 const User = require('../models/user');
 const private = require('../private/private.js');
@@ -78,6 +79,14 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg 
+    });
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {

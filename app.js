04265425ca4bc1cb68660920732e0688e1249b85
stefaleon <stefaleon@gmail.com>
2019-03-02 02:36:14 +0200

    error handling

--- app.js
+++ app.js
@@ -45,32 +45,46 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then(user => {
-      // define the req.user
-      req.user = user;
-      console.log('Current Session User Email is:', user.email);
-      next();
-    })
-    .catch(err => console.log(err));
-});
-
-
-app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
 });
 
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      console.log('Current Session User Email is:', user.email);
+      next();
+    })
+    .catch(err => {
+      next(new Error(err));
+    });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
 
 mongoose
   .connect(MONGODB_URI)
